79d1dbfe2f3d4e9edacb19d7453872cea4cdf777
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-08-18 12:03:28 -0700

    update background gif

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -8,61 +8,50 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
-  String token="";
+  String token = "";
   @override
-  void initState()  {
+  void initState() {
     super.initState();
-     getToken();
+    getToken();
   }
 
-getToken() async{
- final SharedPreferences prefs = await SharedPreferences.getInstance();
-       
-         setState(() {
-            token= prefs.getString('token').toString();
-            
-             Timer(
-            Duration(seconds: 3),
-            () => Navigator.pushNamed(context, '/home'),
-          );
-            // Timer(
-            // Duration(seconds: 3),
-            // () => Navigator.pushNamed(context, '/new/feeds'),
-            // );
-            print("thistok"+token);
-                  if(token.length>1 && token!="null" ){
-              Timer(
-                  Duration(seconds: 3),
-                  () => Navigator.pushNamed(context, '/new/feeds'),
-                  );
-                  }
-                  else {
-                    Timer(
-                  Duration(seconds: 3),
-                  () => Navigator.pushNamed(context, '/home'),
-                );
-                  }
-         });
-     
-}
+  getToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      token = prefs.getString('token').toString();
+
+      Timer(
+        Duration(seconds: 9),
+        () => Navigator.pushNamed(context, '/home'),
+      );
+      // Timer(
+      // Duration(seconds: 3),
+      // () => Navigator.pushNamed(context, '/new/feeds'),
+      // );
+      print("thistok" + token);
+      if (token.length > 1 && token != "null") {
+        Timer(
+          Duration(seconds: 9),
+          () => Navigator.pushNamed(context, '/new/feeds'),
+        );
+      } else {
+        Timer(
+          Duration(seconds: 9),
+          () => Navigator.pushNamed(context, '/home'),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: new Stack(
       children: <Widget>[
         new Container(
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage("assets/images/background.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        new Center(
-          child: Image.asset('assets/images/logo.png',
-              height: 150, width: 150, fit: BoxFit.contain),
-        )
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset('/images/logo.gif', fit: BoxFit.cover)),
       ],
     ));
   }
